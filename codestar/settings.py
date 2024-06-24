8a695b16b5cbd9f5d4bd64a2c61e842cbcc8c80b
Ren Pena <rennielcpena@gmail.com>
2024-06-24 16:46:30 +0100

    Add procfile for gunicorn

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*=ojmlt2z=@0gd58brvlbh^fqjox_!t%k*ex4#9v+)7f57=f64
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
